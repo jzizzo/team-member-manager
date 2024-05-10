d002756748f9b3166ca067b6c8d773418604480b
Joseph Zizzo <hire.jzizzo@gmail.com>
2024-05-10 08:51:10 -0700

    Add TeamMember model

--- team_app/models.py
+++ team_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class TeamMember(models.Model):
+    ROLE_CHOICES = (
+        ("admin", "Admin"),
+        ("teammate", "Teammate"),
+    )
+
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    phone_number = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=12, choices=ROLE_CHOICES, default="teammate")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.role}"
